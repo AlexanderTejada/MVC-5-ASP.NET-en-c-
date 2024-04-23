1199e72e3e2ca643def4408b0d68e6dca1bf39d6
AlexanderTejada <120703301+AlexanderTejada@users.noreply.github.com>
2024-04-23 11:27:48 -0300

    c_2

---
+++ Nueva carpeta/customersApi/customersApi/Controllers/customerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using customersApi.Dtos;
+namespace customersApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class customerController : Controller
+    {
+        [HttpGet()]
+        public async Task<List<CustomersDto> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+        [HttpGet("{id}")]
+      public async Task<CustomersDto>GetCustomers(long id)
+        {
+            throw new NotImplementedException();
+        }
+    [HttpDelete]
+
+    }
+}
---
+++ Nueva carpeta/customersApi/customersApi/Dtos/Customers.cs
@@ -0,0 +1,12 @@
+﻿namespace customersApi.Dtos
+{
+    public class CustomersDto
+    {
+        public long id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }    
+        public string Email { get; set; }   
+        public string Phone { get; set; }
+        public string Adress { get; set; }
+    }
+}
